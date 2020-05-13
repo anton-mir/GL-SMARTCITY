69509f5f726e29e0221fab430e6d2df5fdb90a30
creci <creci@mail.ru>
2020-05-13 11:16:07 +0300

    Xlgyro fake update (#13)
    * Extra lines removed. The warning has been removed.
    
    * Updated code structure.

--- sbc-platform/src/pits/xlgyro_fake/main.c
+++ sbc-platform/src/pits/xlgyro_fake/main.c
@@ -13,8 +13,12 @@
 #define PACKET_PREAMBULE            (0xAA55)
 #define TX_BUF_SIZE                 (1024 * 6)
 #define FIELD_SIZEOF(t, f)          (sizeof(((t*)0)->f))
-#define LSM9DS1_SLIDING_WINDOW_SIZE             (32)
 #define LSM9DS1_I2C_WRITE_BUFFER_SIZE           (32)
+#define DATA_PACKET_LEN_NO_CRC(itms)  ( \
+                                            FIELD_SIZEOF(DATA_PACKET_S, preambule1) + \
+                                            FIELD_SIZEOF(DATA_PACKET_S, preambule2) + \
+                                            FIELD_SIZEOF(DATA_PACKET_S, readySamplesNum) + \
+                                            sizeof(RAW_DATA_S) * itms * 2 )
 typedef enum
 {
     E_X_AXIS = 0,
@@ -22,10 +26,12 @@ typedef enum
     E_Z_AXIS,
     E_AXIS_COUNT
 } AXISES;
+
 typedef struct RAW_DATA_STRUCT
 {
     int16_t rawData[E_AXIS_COUNT];
 } RAW_DATA_S;
+
 typedef struct __attribute__((packed, aligned(1))) DATA_PACKET_STRUCT
 {
     uint16_t preambule1;
@@ -37,28 +43,29 @@ typedef struct __attribute__((packed, aligned(1))) DATA_PACKET_STRUCT
         RAW_DATA_S gValue;
     } bufs[DATA_BUF_SIZE];
 } DATA_PACKET_S;
+
 typedef struct __attribute__((packed, aligned(1))) CIRCULAR_BUF_STRUCT
 {
     uint32_t activeBufIdx;
     DATA_PACKET_S payload[DATA_BUFS_NUM];
 } CIRCULAR_BUF_S;
-CIRCULAR_BUF_S data = { 0 };
-#define DATA_PACKET_LEN_NO_CRC(itms)  ( \
-                                            FIELD_SIZEOF(DATA_PACKET_S, preambule1) + \
-                                            FIELD_SIZEOF(DATA_PACKET_S, preambule2) + \
-                                            FIELD_SIZEOF(DATA_PACKET_S, readySamplesNum) + \
-                                            sizeof(RAW_DATA_S) * itms * 2 )
+
+
 typedef void (*RAW_DATA_APPEND_FP)(RAW_DATA_S *pAccelValue, RAW_DATA_S *pgyroValue);
-typedef struct RAW_SLIDING_WINDOW_STRUCT
-{
-    RAW_DATA_S window[LSM9DS1_SLIDING_WINDOW_SIZE];
-    int32_t windowSum[E_AXIS_COUNT];
-    bool inited;
-} RAW_SLIDING_WINDOW_S;
-static RAW_SLIDING_WINDOW_S xlSlidingWindow = { 0 };
-static RAW_SLIDING_WINDOW_S gSlidingWindow = { 0 };
-uint8_t txBuf[TX_BUF_SIZE] = { 0 };
-int fd;/*File Descriptor*/
+
+static uint8_t txBuf[TX_BUF_SIZE] = { 0 };
+static int fd;/*File Descriptor*/
+static CIRCULAR_BUF_S data = { 0 };
+
+void DataBufValuesAppend(RAW_DATA_S *pAccelValue, RAW_DATA_S *pGyroValue);
+static bool Open_Port(char *port);
+static void Close_Port();
+static void send (uint8_t *pData, uint16_t Size);
+static void dataBufSend(CIRCULAR_BUF_S *pData);
+void LSM9DS1_AccelReadRawData(RAW_DATA_S *pRawData);
+void  LSM9DS1_GyroReadRawData(RAW_DATA_S *pRawData);
+static uint32_t LSM9DS1_PollDataBlocking(RAW_DATA_APPEND_FP fpDataAppendCallback);
+
 void DataBufValuesAppend(RAW_DATA_S *pAccelValue, RAW_DATA_S *pGyroValue)
 {
     const uint32_t idx = data.activeBufIdx;
@@ -79,7 +86,7 @@ void DataBufValuesAppend(RAW_DATA_S *pAccelValue, RAW_DATA_S *pGyroValue)
                 sizeof(RAW_DATA_S));
 
             data.payload[idx].readySamplesNum++;
-        }        
+        }
     }
 }
 
@@ -89,7 +96,7 @@ static bool Open_Port(char *port){
     printf("\n +----------------------------------+");
 
     /*------------------------------- Opening the Serial Port -------------------------------*/
-    fd = open(port,O_RDWR | O_NOCTTY | O_NDELAY);	
+    fd = open(port,O_RDWR | O_NOCTTY | O_NDELAY);
     /* O_RDWR Read/Write access to serial port           */
     /* O_NOCTTY - No terminal will control the process   */
     /* O_NDELAY -Non Blocking Mode,Does not care about-  */
@@ -144,15 +151,18 @@ static bool Open_Port(char *port){
     config.c_cc[VTIME] = 15;    // 15 * 10ms = 150ms
 
     cfsetispeed(&config, B115200);
+    return 0;
 
 }
+
 static void Close_Port(){
-        close(fd);/* Close the Serial port */
+    close(fd);/* Close the Serial port */
 }
+
 static void send (uint8_t *pData, uint16_t Size){
 
     /*------------------------------- Write data to serial port -----------------------------*/
-     write(fd,pData,Size);/* use write() to send data to port                                            */
+    write(fd,pData,Size);/* use write() to send data to port                                            */
     /* "fd"                   - file descriptor pointing to the opened serial port */
     /*	"write_buffer"         - address of the buffer containing data	            */
     /* "sizeof(write_buffer)" - No of bytes to write                               */
@@ -161,6 +171,7 @@ static void send (uint8_t *pData, uint16_t Size){
 
 
 }
+
 static void dataBufSend(CIRCULAR_BUF_S *pData)
 {
     const uint32_t idx = pData->activeBufIdx;
@@ -196,6 +207,7 @@ static void dataBufSend(CIRCULAR_BUF_S *pData)
         return ;
     }
 }
+
 void LSM9DS1_AccelReadRawData(RAW_DATA_S *pRawData)
 {
     uint8_t i2cBuf[LSM9DS1_I2C_WRITE_BUFFER_SIZE];
@@ -214,6 +226,7 @@ void LSM9DS1_AccelReadRawData(RAW_DATA_S *pRawData)
     }while(0);
 
 }
+
 void  LSM9DS1_GyroReadRawData(RAW_DATA_S *pRawData)
 {
     uint8_t i2cBuf[LSM9DS1_I2C_WRITE_BUFFER_SIZE];
@@ -233,43 +246,7 @@ void  LSM9DS1_GyroReadRawData(RAW_DATA_S *pRawData)
     }while(0);
 
 }
-static void LSM9DS1_slidingWindowPush(RAW_SLIDING_WINDOW_S *pWindow, RAW_DATA_S *pValue)
-{
-    pWindow->windowSum[E_X_AXIS] = 0;
-    pWindow->windowSum[E_Y_AXIS] = 0;
-    pWindow->windowSum[E_Z_AXIS] = 0;
 
-    if (pWindow->inited != true)
-    {
-        /* If sliding window is not inited - fill it with frist given value */
-        for (uint32_t i = 0; i < LSM9DS1_SLIDING_WINDOW_SIZE; ++i)
-        {
-            memcpy(&pWindow->window[i], pValue, sizeof(RAW_DATA_S));
-
-            pWindow->windowSum[E_X_AXIS] += pWindow->window[i].rawData[E_X_AXIS];
-            pWindow->windowSum[E_Y_AXIS] += pWindow->window[i].rawData[E_Y_AXIS];
-            pWindow->windowSum[E_Z_AXIS] += pWindow->window[i].rawData[E_Z_AXIS];
-        }
-
-        pWindow->inited = true;
-    }
-    else
-    {
-        for (uint32_t i = (LSM9DS1_SLIDING_WINDOW_SIZE - 1); i > 0; --i)
-        {
-            pWindow->window[i] = pWindow->window[i - 1];
-            pWindow->windowSum[E_X_AXIS] += pWindow->window[i].rawData[E_X_AXIS];
-            pWindow->windowSum[E_Y_AXIS] += pWindow->window[i].rawData[E_Y_AXIS];
-            pWindow->windowSum[E_Z_AXIS] += pWindow->window[i].rawData[E_Z_AXIS];
-        }
-
-        memcpy(&pWindow->window[0], pValue, sizeof(RAW_DATA_S));
-
-        pWindow->windowSum[E_X_AXIS] += pWindow->window[0].rawData[E_X_AXIS];
-        pWindow->windowSum[E_Y_AXIS] += pWindow->window[0].rawData[E_Y_AXIS];
-        pWindow->windowSum[E_Z_AXIS] += pWindow->window[0].rawData[E_Z_AXIS];
-    }
-}
 static uint32_t LSM9DS1_PollDataBlocking(RAW_DATA_APPEND_FP fpDataAppendCallback)
 {
     uint32_t ret = 0;
@@ -290,9 +267,6 @@ static uint32_t LSM9DS1_PollDataBlocking(RAW_DATA_APPEND_FP fpDataAppendCallback
 
                 LSM9DS1_GyroReadRawData(&gyroData);
 
-                LSM9DS1_slidingWindowPush(&xlSlidingWindow, &accelData);
-                LSM9DS1_slidingWindowPush(&gSlidingWindow, &gyroData);
-
                 if (fpDataAppendCallback != NULL)
                 {
                     fpDataAppendCallback(&accelData, &gyroData);
@@ -308,17 +282,17 @@ static uint32_t LSM9DS1_PollDataBlocking(RAW_DATA_APPEND_FP fpDataAppendCallback
 
 int main(int argc, char * argv[])
 {
-    if(argc==2){        
-    if(Open_Port(argv[1])==0){
-    while(1){
-        (void)LSM9DS1_PollDataBlocking(DataBufValuesAppend);
-        dataBufSend(&data);
-        sleep(1);
-    }
-    Close_Port();
-    }
+    if(argc==2){
+        if(Open_Port(argv[1])==0){
+            while(1){
+                (void)LSM9DS1_PollDataBlocking(DataBufValuesAppend);
+                dataBufSend(&data);
+                sleep(1);
+            }
+            Close_Port();
+        }
     }else{
-    printf("Example: ./xlgyro_fake_input /dev/ttyUSB0\n When \"/dev/ttyUSB0\" - any port\n");    
+        printf("Example: ./xlgyro_fake_input /dev/ttyUSB0\n When \"/dev/ttyUSB0\" - any port\n");
     }
 }
 
