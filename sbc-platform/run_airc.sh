7202fe2747a7f2cbcf4ed94d95af73935b5862f8
Airc <airc@airc.com>
2020-12-17 13:12:11 +0200

    Fix run_airc.sh for auto-run all stuff together

--- sbc-platform/run_airc.sh
+++ sbc-platform/run_airc.sh
@@ -1,12 +1,22 @@
-sh stop_airc.sh > /dev/null
+#!/bin/bash
+
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo " *** Trapped CTRL-C, exitting and stopping airc..."
+	sudo ../stop_airc.sh
+}
+
+#sh stop_airc.sh > /dev/null
 mkdir -p logs
 
-echo -en "Running GPS simulation...\n"
-bash -c 'gpsfake -c 1 ~/src/GL-SMARTCITY/sbc-platform/car_path_simulation.nmea &' >> logs/gpsfake.log 2>> logs/gpsfake.err
-sleep 1
+#echo -en "Running GPS simulation...\n"
+#bash -c 'gpsfake -c 1 ~/src/GL-SMARTCITY/sbc-platform/car_path_simulation.nmea &' >> logs/gpsfake.log 2>> logs/gpsfake.err
+#sleep 1
 
 echo -en "\nRunning obdgpslogger...\n"
-bash -c 'cd ~/src/GL-SMARTCITY/sbc-platform/build/src/sbc-car/bin && ./obdgpslogger -x box &' >> logs/obdgpslogger.log 2>> logs/obdgpslogger.err
+bash -c 'cd ~/src/GL-SMARTCITY/sbc-platform/build/src/sbc-car/bin && ./obdgpslogger -x box --airc-device box --airc-box-ip 192.168.1.121 &' >> logs/obdgpslogger.log 2>> logs/obdgpslogger.err
 sleep 3
 
 echo -en "\nRunning car cluster...\n"
@@ -20,3 +30,7 @@ sleep 3
 cd logs
 bash -c 'while true; do tail -10 cloud_hub.log > cloud_hub_.log; cat cloud_hub_.log > cloud_hub.log; tail -10 cluster-app.err > cluster-app_.err; cat cluster-app_.err > cluster-app.err; tail -10 obdgpslogger.log > obdgpslogger_.log; cat obdgpslogger_.log > obdgpslogger.log; sleep 300; done;' &
 echo $! > log_ps_id
+
+echo -en "!!!! ============>> FOR EXIT PLEASE PRESS Ctrl-C <<============= !!!!\n\n"
+
+tail -f cloud_hub.log
